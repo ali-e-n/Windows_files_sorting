8aabb6f921903cced455def6ab687f7e88e8ab3d
Ali Akbar khan <aliakbarkhan161@gmail.com>
2023-04-01 22:28:21 +0500

    Comments added

--- filesorter.py
+++ filesorter.py
@@ -1,11 +1,15 @@
-import os
-import tkinter as tk
-from tkinter import messagebox
+import os  # Import the os module
+import tkinter as tk # Import the tkinter module
+from tkinter import messagebox # Import the messagebox module
 
+# Create the root window
 root = tk.Tk()
+# Set the size of the root window
 root.geometry("300x200")
+# Set the title of the root window
 root.title("File Sorter")
 
+# Define the sort_files function
 def sort_files():
     download_dir = os.path.join(os.path.expanduser("~"), "Downloads")
     for file_name in os.listdir(download_dir):
@@ -17,6 +21,7 @@ def sort_files():
             os.rename(os.path.join(download_dir, file_name), os.path.join(download_dir, file_extension, file_name))
     messagebox.showinfo("File Sorter", "Files sorted successfully!")
 
+# Create the sort button
 sort_button = tk.Button(root, text="Sort Files", command=sort_files)
 sort_button.pack(pady=20)
 
